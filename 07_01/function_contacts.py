f3b660d7ba1adc73e407a571bbe48f954ec3afa8
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2025-03-19 09:46:14 +0100

    update function_contacts.py

--- 07_01/function_contacts.py
+++ 07_01/function_contacts.py
@@ -27,17 +27,30 @@ def parse_input(user_input):
     return cmd, *args
 
 
+# @input_error
+# def add_contact(args, contacts):
+#     name, phone = args
+#     if name in contacts:
+#         user_input = input("Contact already exists. Do you want to update it? (yes/no): ")
+#         if user_input.strip().lower() == "yes":
+#             return change_contact(args, contacts)
+#         else:
+#             return "Contact not added."
+#     contacts[name] = phone
+#     return "Contact added."
+
 @input_error
-def add_contact(args, contacts):
-    name, phone = args
-    if name in contacts:
-        user_input = input("Contact already exists. Do you want to update it? (yes/no): ")
-        if user_input.strip().lower() == "yes":
-            return change_contact(args, contacts)
-        else:
-            return "Contact not added."
-    contacts[name] = phone
-    return "Contact added."
+def add_contact(args, book: AddressBook):
+    name, phone, *_ = args
+    record = book.find(name)
+    message = "Contact updated."
+    if record is None:
+        record = Record(name)
+        book.add_record(record)
+        message = "Contact added."
+    if phone:
+        record.add_phone(phone)
+    return message
 
 
 @input_error
